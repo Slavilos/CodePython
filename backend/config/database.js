a6887ca22e1fedae5ac34506e53b08cdb4566735
Svyatoslav <124680646+Slavilos@users.noreply.github.com>
2025-05-23 10:57:30 +0500

    database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,8 @@
 export const dbConfig = {
   database: 'codepython',
-  username: 'root',
-  password: '',       
-  host: 'localhost',
-  port: 3307,          
+  username: 'codepython',
+  password: '7KsycuFIlChyG20FuUuQqodadkwYZ4LB',       
+  host: 'dpg-d0o0o7ali9vc73fhqe20-a',
+  port: 5432,          
   dialect: 'mysql'
 };
